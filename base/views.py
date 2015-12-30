238482d5ec195f38a557491da50e346a6514eb85
John Robinson <soco@uw.edu>
2015-12-30 00:32:47 -0800

    update spacing and formatting

--- base/views.py
+++ base/views.py
@@ -7,33 +7,32 @@ from django.contrib.auth.decorators import login_required
 
 class LoginRequiredMixin(object):
     """A mixin that forces a login to view the CBTemplate."""
-    
+
     @classmethod
     def as_view(cls, **initkwargs):
         view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
         return login_required(view)
 
 
-
 class AjaxableResponseMixin(object):
-	"""
-	Mixin to add AJAX support to a form.
-	Must be used with an object-based FormView (e.g. CreateView)
-	"""
-	def form_invalid(self, form):
-		response = super(AjaxableResponseMixin, self).form_invalid(form)
-		return JsonResponse(form.errors, status=400)
+    """
+    Mixin to add AJAX support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    """
+    def form_invalid(self, form):
+        response = super(AjaxableResponseMixin, self).form_invalid(form)
+        return JsonResponse(form.errors, status=400)
 
 
-	def form_valid(self, form):
-		# We make sure to call the parent's form_valid() method because
-		# it might do some processing (in the case of CreateView, it will
-		# call form.save() for example).
-		logger.info("form is valid")
+    def form_valid(self, form):
+        # We make sure to call the parent's form_valid() method because
+        # it might do some processing (in the case of CreateView, it will
+        # call form.save() for example).
+        logger.info("form is valid")
 
-		# data = serializers.serialize('json', form)
-		#logger.info("form data: %s"%(str(form.fields['text'])))
-		response = super(AjaxableResponseMixin, self).form_valid(form)
-		data = model_to_dict(self.object)
-		return JsonResponse(data)
+        # data = serializers.serialize('json', form)
+        #logger.info("form data: %s"%(str(form.fields['text'])))
+        response = super(AjaxableResponseMixin, self).form_valid(form)
+        data = model_to_dict(self.object)
+        return JsonResponse(data)
 
