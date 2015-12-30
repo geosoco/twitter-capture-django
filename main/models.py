f645787b15f0e03d2c42b81dacc9485662dd2e4a
John Robinson <soco@uw.edu>
2015-12-30 00:04:09 -0800

    reformatting and comments

--- main/models.py
+++ main/models.py
@@ -16,126 +16,127 @@ from datetime import datetime
 logger = logging.getLogger(__name__)
 
 
-
-	
+#
+# classes
+#
 
 
 class Job(FullAuditModel):
-	"""
-		Model for a capture
-	"""
-	STATUS_UNKNOWN = 0
-	STATUS_CREATED = 1
-	STATUS_STARTING = 2
-	STATUS_STARTED = 3
-	STATUS_STOPPING = 4
-	STATUS_STOPPED = 5
+    """
+        Model for a capture
+    """
+    STATUS_UNKNOWN = 0
+    STATUS_CREATED = 1
+    STATUS_STARTING = 2
+    STATUS_STARTED = 3
+    STATUS_STOPPING = 4
+    STATUS_STOPPED = 5
 
 
-	STATUS_CHOICES = (
-		(STATUS_UNKNOWN, 'unknown'),
-		(STATUS_CREATED, 'created'),
-		(STATUS_STARTING, 'starting'),
-		(STATUS_STARTED, 'started'),
-		(STATUS_STOPPING, 'stopping'),
-		(STATUS_STOPPED, 'stopped'),
-	)
+    STATUS_CHOICES = (
+        (STATUS_UNKNOWN, 'unknown'),
+        (STATUS_CREATED, 'created'),
+        (STATUS_STARTING, 'starting'),
+        (STATUS_STARTED, 'started'),
+        (STATUS_STOPPING, 'stopping'),
+        (STATUS_STOPPED, 'stopped'),
+    )
 
-	alphanumeric_start = RegexValidator(regex='^[0-9a-zA-Z_\-\ ]+', message='Must start with an alphanumeric character', code='alpha_start')
-	alphanumeric = RegexValidator(r'^[0-9a-zA-Z_\-\ ]*$', 'Only alphanumeric characters, spaces, and _ and - are allowed.')
+    alphanumeric_start = RegexValidator(regex='^[0-9a-zA-Z_\-\ ]+', message='Must start with an alphanumeric character', code='alpha_start')
+    alphanumeric = RegexValidator(r'^[0-9a-zA-Z_\-\ ]*$', 'Only alphanumeric characters, spaces, and _ and - are allowed.')
 
 
-	name = models.CharField(max_length=64, blank=False, null=False, validators=[alphanumeric,alphanumeric_start], unique=True, 
-		help_text="A unique name for this capture. Can only contain alpha-numeric characters, spaces, dashes (-), and underscores (_). ")
-	description = models.TextField(
-		help_text="A detailed description of the event. Please add possible rumors to this as the event unfolds.")
+    name = models.CharField(max_length=64, blank=False, null=False, validators=[alphanumeric,alphanumeric_start], unique=True, 
+        help_text="A unique name for this capture. Can only contain alpha-numeric characters, spaces, dashes (-), and underscores (_). ")
+    description = models.TextField(
+        help_text="A detailed description of the event. Please add possible rumors to this as the event unfolds.")
 
-	twitter_keywords = models.TextField(
-		help_text="A comma separated list of terms. eg. term1, term2, term3")
+    twitter_keywords = models.TextField(
+        help_text="A comma separated list of terms. eg. term1, term2, term3")
 
-	status = models.IntegerField(choices=STATUS_CHOICES, default=STATUS_CREATED)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=STATUS_CREATED)
 
-	task_id = models.IntegerField(null=True, blank=True)
+    task_id = models.IntegerField(null=True, blank=True)
 
-	first_started = models.DateTimeField(null=True, blank=True)
-	started = models.DateTimeField(null=True, blank=True)
-	stopped = models.DateTimeField(null=True, blank=True)
+    first_started = models.DateTimeField(null=True, blank=True)
+    started = models.DateTimeField(null=True, blank=True)
+    stopped = models.DateTimeField(null=True, blank=True)
 
-	archived_date = models.DateTimeField(null=True, blank=True)
-	archived_by = models.ForeignKey(User, null=True, blank=True, default=None, related_name='job_archived_by')
+    archived_date = models.DateTimeField(null=True, blank=True)
+    archived_by = models.ForeignKey(User, null=True, blank=True, default=None, related_name='job_archived_by')
 
-	total_count = models.IntegerField(null=True, blank=True)
-	rate = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
-	ping_date = models.DateTimeField(null=True, blank=True)
+    total_count = models.IntegerField(null=True, blank=True)
+    rate = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
+    ping_date = models.DateTimeField(null=True, blank=True)
 
-	assigned_worker = models.ForeignKey(User, null=True, blank=True, default=None,
-		related_name = 'job_assigned_worker',
-		help_text="This list will only display unassigned capture clients. If there are none, you must archive an existing capture.")
+    assigned_worker = models.ForeignKey(
+        User, null=True, blank=True, default=None,
+        related_name='job_assigned_worker',
+        help_text="This list will only display unassigned capture clients. If there are none, you must archive an existing capture.")
 
-	def get_absolute_url(self):
-		return reverse('capture-details', kwargs={ 'pk': self.pk, })
-
+    def get_absolute_url(self):
+        return reverse('capture-details', kwargs={'pk': self.pk, })
 
 
 @receiver(pre_save, sender=Job)
-def on_job_pre_save(sender, instance, *args, **kwargs):	
-	if instance.id:
-		old_job = Job.objects.get(pk=instance.id)
+def on_job_pre_save(sender, instance, *args, **kwargs):
+    if instance.id:
+        old_job = Job.objects.get(pk=instance.id)
 
-		# if we're archiving it, stop it
-		if instance.archived_date is not None and old_job.archived_date != instance.archived_date:
-			instance.status = Job.STATUS_STOPPED
+        # if we're archiving it, stop it
+        if instance.archived_date is not None and old_job.archived_date != instance.archived_date:
+            instance.status = Job.STATUS_STOPPED
 
 
-		# has the status changed?
-		if old_job.status != instance.status:
-			# if status is changing
-			if instance.status == Job.STATUS_STARTED:
-				# first started
-				if instance.first_started == None:
-					instance.first_started = datetime.now()
-				# now any start value
-				instance.started = datetime.now()
-			elif instance.status == Job.STATUS_STOPPED:
-				instance.stopped = datetime.now()
+        # has the status changed?
+        if old_job.status != instance.status:
+            # if status is changing
+            if instance.status == Job.STATUS_STARTED:
+                # first started
+                if instance.first_started == None:
+                    instance.first_started = datetime.now()
+                # now any start value
+                instance.started = datetime.now()
+            elif instance.status == Job.STATUS_STOPPED:
+                instance.stopped = datetime.now()
 
-		# 
+        # 
 
 
 
 
 class JobModification(ModifiedByMixin):
-	"""
-		Model for change history of a capturejob 
-	"""
-	changes = models.TextField()
+    """
+        Model for change history of a capturejob 
+    """
+    changes = models.TextField()
 
-	job = models.ForeignKey(Job, blank=True)
+    job = models.ForeignKey(Job, blank=True)
 
 
 
 class Update(models.Model):
-	"""
-		Model for capture update / update 
-	"""
-	job = models.ForeignKey(Job)
-	date = models.DateTimeField(auto_now_add=True)
+    """
+        Model for capture update / update 
+    """
+    job = models.ForeignKey(Job)
+    date = models.DateTimeField(auto_now_add=True)
 
-	count = models.IntegerField(null=True, blank=True)
-	total_count = models.IntegerField(null=True, blank=True)
-	rate = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
+    count = models.IntegerField(null=True, blank=True)
+    total_count = models.IntegerField(null=True, blank=True)
+    rate = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
 
 
 class LogMessage(models.Model):
-	"""
-		Model for log messages from client
-	"""
-	job = models.ForeignKey(Job)
-	date = models.DateTimeField(auto_now_add=True)
+    """
+        Model for log messages from client
+    """
+    job = models.ForeignKey(Job)
+    date = models.DateTimeField(auto_now_add=True)
 
-	source = models.CharField(max_length=32, blank=True, null=True)
-	kind = models.CharField(max_length=4, blank=True, null=True)
-	message = models.TextField()
+    source = models.CharField(max_length=32, blank=True, null=True)
+    kind = models.CharField(max_length=4, blank=True, null=True)
+    message = models.TextField()
 
 
 
