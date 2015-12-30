fee1576f2160d94353680e2ba3f4c07b9e03be0a
John Robinson <soco@uw.edu>
2015-12-30 00:34:51 -0800

    fix formatting, move to timezone instead of datetime.now, fix list command for update

--- api_auth/views.py
+++ api_auth/views.py
@@ -1,18 +1,18 @@
 from django.contrib.auth.models import User, Group
-from main.models import Job, JobModification, Update
+from main.models import Job, Update
 from worker.models import Worker
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.authentication import (
+    SessionAuthentication, BasicAuthentication, TokenAuthentication)
 from rest_framework.permissions import IsAuthenticated
-from api_auth.serializers import *
-from rest_framework import filters
+from api_auth.serializers import (
+    UserSerializer, GroupSerializer, JobSerializer, ClientSerializer,
+    UpdateSerializer, WorkerSerializer)
 import django_filters
 from datetime import datetime
 from django.db import connection
-import simplejson as json
-
-
+from django.utils import timezone
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -21,7 +21,8 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    authentication_classes = (SessionAuthentication,
+                              BasicAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
 
     def dispatch(self, request, *args, **kwargs):
@@ -31,22 +32,24 @@ class UserViewSet(viewsets.ModelViewSet):
         return super(UserViewSet, self).dispatch(request, *args, **kwargs)
 
 
-
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    authentication_classes = (SessionAuthentication,
+                              BasicAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
 
 
 class JobFilterSet(django_filters.FilterSet):
     active = django_filters.BooleanFilter(name="archived_date__isnull")
+
     class Meta:
         model = Job
         fields = ['active']
+
 
 class JobViewSet(viewsets.ModelViewSet):
     """
@@ -54,20 +57,20 @@ class JobViewSet(viewsets.ModelViewSet):
     """
     queryset = Job.objects.all()
     serializer_class = JobSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    authentication_classes = (SessionAuthentication,
+                              BasicAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
     filter_class = JobFilterSet
 
     def perform_create(self, serializer):
         serializer.created_by = self.request.user
-        serializer.created_date = datetime.now()
+        serializer.created_date = timezone.now()
         serializer.save()
 
     def perform_destroy(self, serializer):
         serializer.deleted_by = self.request.user
-        serializer.deleted_date = datetime.now()
+        serializer.deleted_date = timezone.now()
         serializer.save()
-
 
     def has_value_changed(self, instance, data, name):
         if name in data:
@@ -80,24 +83,23 @@ class JobViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         kwargs = {}
         serializer.modified_by = self.request.user
-        serializer.modified_date = datetime.now()
+        serializer.modified_date = timezone.now()
 
         # grab objects to compare
         instance = self.get_object()
         data = serializer.validated_data
 
         if self.has_value_changed(instance, data, 'archived_date'):
-            if 'archived_date' in  data and data['archived_date'] is not None:
+            if 'archived_date' in data and data['archived_date'] is not None:
                 serializer.validated_data['archived_by'] = self.request.user
 
-        
         # set ping date if right item
-        if instance.assigned_worker is not None and self.request.user.id == instance.assigned_worker.id:
-            data['ping_date'] = datetime.now()
-            kwargs['ping_date'] = datetime.now()
+        if (instance.assigned_worker is not None and
+                self.request.user.id == instance.assigned_worker.id):
+            data['ping_date'] = timezone.now()
+            kwargs['ping_date'] = timezone.now()
 
         serializer.save()
-
 
 
 class ActiveJobViewSet(viewsets.ModelViewSet):
@@ -105,13 +107,14 @@ class ActiveJobViewSet(viewsets.ModelViewSet):
     API endpoint for active jobs of the current user
     """
     serializer_class = JobSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    authentication_classes = (SessionAuthentication,
+                              BasicAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        qs = Job.objects.filter(assigned_worker=self.request.user).exclude(archived_date__isnull=False)
+        qs = Job.objects.filter(assigned_worker=self.request.user).exclude(
+            archived_date__isnull=False)
         return qs
-
 
 
 class UpdateViewSet(viewsets.ModelViewSet):
@@ -120,7 +123,8 @@ class UpdateViewSet(viewsets.ModelViewSet):
     """
     queryset = Update.objects.all()
     serializer_class = UpdateSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    authentication_classes = (SessionAuthentication,
+                              BasicAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
 
 
@@ -130,7 +134,8 @@ class ClientViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.filter(groups__name='capture_client')
     serializer_class = ClientSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    authentication_classes = (SessionAuthentication,
+                              BasicAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
 
 
@@ -140,7 +145,8 @@ class WorkerViewSet(viewsets.ModelViewSet):
     """
     queryset = Worker.objects.all()
     serializer_class = WorkerSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    authentication_classes = (SessionAuthentication,
+                              BasicAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
 
 
@@ -170,8 +176,7 @@ class UpdateViewSet2(viewsets.ViewSet):
 
         return Response(result.values())
 
-
-    def run_query(self, request, query, params = None):
+    def run_query(self, request, query, params=None):
         cursor = connection.cursor()
         cursor.execute(query, params)
 
@@ -181,22 +186,34 @@ class UpdateViewSet2(viewsets.ViewSet):
 
         return result
 
-
-    def list(self,request):
+    def list(self, request, format=None):
         query = """
-        select j.id as job_id, from_unixtime(FLOOR(unix_timestamp(date)/60)*60) as date, FLOOR(unix_timestamp(date)/60)*60 as unixdate, sum(count) as cnt, round(avg(count),1) as average
+        select
+            j.id as job_id,
+            from_unixtime(FLOOR(unix_timestamp(date)/60)*60) as date,
+            FLOOR(unix_timestamp(date)/60)*60 as unixdate, sum(count) as cnt,
+            round(avg(count),1) as average
         from capture.main_update
         inner join capture.main_job j on j.id = job_id
-        where ISNULL(j.archived_date) and date > DATE_SUB(utc_timestamp(), INTERVAL 1 HOUR)
-        group by j.id, unixdate; 
+        where
+            ISNULL(j.archived_date) and
+            date > DATE_SUB(utc_timestamp(), INTERVAL 1 HOUR)
+        group by j.id, unixdate;
         """
         return self.run_query(request, query)
 
-    def retrieve(self, request, pk=None):
+    def retrieve(self, request, pk=None, format=None):
         query = """
-        select job_id, from_unixtime(FLOOR(unix_timestamp(date)/60)*60) as date, FLOOR(unix_timestamp(date)/60)*60 as unixdate, sum(count) as cnt, round(avg(count),1) as average
+        select
+            job_id,
+            from_unixtime(FLOOR(unix_timestamp(date)/60)*60) as date,
+            FLOOR(unix_timestamp(date)/60)*60 as unixdate,
+            sum(count) as cnt,
+            round(avg(count),1) as average
         from capture.main_update
-        where job_id = %s and date > DATE_SUB(utc_timestamp(), INTERVAL 1 HOUR)
+        where
+            job_id = %s and
+            date > DATE_SUB(utc_timestamp(), INTERVAL 1 HOUR)
         group by unixdate;
         """
         return self.run_query(request, query, [pk])
@@ -204,5 +221,3 @@ class UpdateViewSet2(viewsets.ViewSet):
 
 #    def retrieve(self, request, pk=None):
 #        pass
-
-
