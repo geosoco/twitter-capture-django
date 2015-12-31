a274ed50168e0eda006c3c6ebbd3e35ee31998a6
John Robinson <soco@uw.edu>
2015-12-31 15:34:35 -0800

    refactor services

--- main/static/capture/js/services.js
+++ main/static/capture/js/services.js
@@ -1,63 +1,58 @@
 (function() {
 
-	var captureServices = angular.module('capture.services', ['ngResource']);
-
+	var services = angular.module('capture.services', ['ngResource']);
 
 	/*
-	 * Job
+	 * basic service
 	 */
-	captureServices.factory("Job", function($resource){
-		return $resource( "/api/jobs/:id/\/.json", {id: "@id"}, 
-			{
+	var basicService = {
 				query: { 
 					method: 'GET',
-					transformResponse: function(data) { 
-						var results = angular.fromJson(data);
-						return results.results;  
-					},
+					transformResponse: transformGetResponse,
 					isArray: true
 				},
-				update: { method: 'PUT'}
-			});
-	});
-
-	/*
-	 * Client
-	 */
-
-	 captureServices.factory("Client",function($resource){
-		return $resource( "/api/clients/:id/\/.json", {id: "@id"},
-			{
-				query: {
-					method: 'GET',
-					transformResponse: function(data) {
-						var results = angular.fromJson(data);
-						return results.results;
-					},
-					isArray: true
-				},
-				update: { method: 'PUT'}
-			});
-	 });
+				update: { method: 'PATCH'},
+				delete: { method: 'DELETE'},
+				save: { method: 'POST'},
+				get: { method: 'GET'}
+			};
 
 
 	/*
-	 * Update
+	 *
+	 * common function for transforming results
+	 * 
+	 */
+	function transformGetResponse(data) {
+		var results = angular.fromJson(data);
+		return results.results;
+	}
+
+
+	/*
+	 *
+	 * service methods
+	 *
 	 */
 
-	 captureServices.factory("Update",function($resource){
-		return $resource( "/api/liveupdates/:id/\/.json", {id: "@job_id"},
-			{
-				query: {
-					method: 'GET',
-					transformResponse: function(data) {
-						var results = angular.fromJson(data);
-						return results.results;
-					},
-					isArray: true
-				},
-				update: { method: 'PUT'}
-			});
-	 });
+	function JobService($resource) {
+		return $resource( "/api/jobs/:id/\/.json", {id: "@id"}, basicService );
+	}
+
+	function ClientService($resource) {
+		return $resource( "/api/clients/:id/\/.json", {id: "@id"}, basicService );
+	}
+
+	function UpdateService($resource) {
+		return $resource( "/api/liveupdates/:id/\/.json", {id: "@job_id"}, basicService );
+	}
+
+
+
+
+	services.factory("Job", JobService );
+	services.factory("Client", ClientService );
+	services.factory("Update", UpdateService );
+
 
 })();
